23f5777111e30b527952bfa441dfa6e05a2443eb
Aramis Romero Carballea <aramisromero89@gmail.com>
2022-10-08 22:18:23 -0400

    add swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    .setTitle('Gateway manager')
+    .setDescription('Gateway manager API')
+    .build();
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
