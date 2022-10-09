8a404d734eaef76e106ee55f938951f538acf21b
Aramis Romero Carballea <aramisromero89@gmail.com>
2022-10-08 23:26:27 -0400

    fix app port env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  require('dotenv').config()
   const config = new DocumentBuilder()
     .setTitle('Gateway manager')
     .setDescription('Gateway manager API')
@@ -10,6 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  var appPort = process.env.PORT || '3000'
+  await app.listen(Number(appPort));
 }
 bootstrap();
